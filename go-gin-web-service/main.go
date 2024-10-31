7d29e00ef8a8beb8bacdf9dc4cc1f496ac1207eb
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-10-31 12:02:00 +0100

    Add getCocktailsByLetter endpoint

--- go-gin-web-service/main.go
+++ go-gin-web-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/cocktails", getCocktails)
+    router.GET("/cocktails/:letter", getCocktailsByLetter)
 
 	router.Run(":8080")
 }
@@ -19,6 +21,33 @@ func main() {
 func getCocktails(context *gin.Context) {
 	// Call the external API
     response, err := http.Get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+    if err != nil {
+        log.Println("Error calling the API:", err)
+        context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+        return
+	}
+	defer response.Body.Close()
+
+    // Read the response body
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        log.Println("Error reading response body:", err)
+        context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read data"})
+        return
+    }
+
+    // Send the response to the client
+    context.Data(http.StatusOK, "application/json", body)
+}
+
+func getCocktailsByLetter(context *gin.Context) {
+    // Get the letter from te URL parameter
+    letter := context.Param("letter")
+
+    url := fmt.Sprintf("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=%s", letter)
+
+    // Call the external API
+    response, err := http.Get(url)
     if err != nil {
         log.Println("Error calling the API:", err)
         context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
