49ad3ef16e72255d1ce1a0a31ff992799cb8907f
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-10-31 11:31:02 +0100

    Add Go-Gin server with getCocktails endpoint

---
+++ go-gin-web-service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/cocktails", getCocktails)
+
+	router.Run(":8080")
+}
+
+func getCocktails(context *gin.Context) {
+	// Call the external API
+    response, err := http.Get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+    if err != nil {
+        log.Println("Error calling the API:", err)
+        context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+        return
+	}
+	defer response.Body.Close()
+
+    // Read the response body
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        log.Println("Error reading response body:", err)
+        context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read data"})
+        return
+    }
+
+    // Send the response to the client
+    context.Data(http.StatusOK, "application/json", body)
+}
